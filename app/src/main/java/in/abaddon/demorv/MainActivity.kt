a586d264e8e6e71905193dbe4024256926bb64cd
BigAru <arooran@indikon.ch>
2020-10-07 16:55:07 +0200

    set onItemClickListener

--- app/src/main/java/in/abaddon/demorv/MainActivity.kt
+++ app/src/main/java/in/abaddon/demorv/MainActivity.kt
@@ -2,9 +2,10 @@ package `in`.abaddon.demorv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,12 +13,16 @@ class MainActivity : AppCompatActivity() {
 
         val todoRepo = DefaultTodoRepo()
         val initialData = todoRepo.getMany(0, 10)
-        val todoAdapter = TodoAdapter(initialData)
+        val todoAdapter = TodoAdapter(initialData, this)
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
         recyclerView.apply {
             adapter = todoAdapter
             setHasFixedSize(true)
         }
+    }
+
+    override fun onItemClick(item: Todo, position: Int) {
+        Toast.makeText(this, item.description, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/in/abaddon/demorv/TodoAdapter.kt
+++ app/src/main/java/in/abaddon/demorv/TodoAdapter.kt
@@ -7,21 +7,39 @@ import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-data class TodoVH(val root: View): RecyclerView.ViewHolder(root){
+interface OnItemClickListener {
+    fun onItemClick(item: Todo, position: Int)
+}
+
+data class TodoVH(
+    val root: View,
+    val clickListener: OnItemClickListener
+): RecyclerView.ViewHolder(root), View.OnClickListener {
+
     val description: TextView = root.findViewById(R.id.row_description)
     val id: TextView = root.findViewById(R.id.row_id)
     val checkBox: CheckBox = root.findViewById(R.id.row_checkbox)
+    lateinit var todo: Todo
 
-    fun bindData(item: Todo){
-       description.text = item.description
-       id.text = item.id.toString().take(8)
+    init {
+        root.setOnClickListener(this)
+    }
+
+    fun bindData(data: Todo){
+        description.text = data.description
+        id.text = data.id.toString().take(8)
+        todo = data
+    }
+
+    override fun onClick(v: View?) {
+        clickListener.onItemClick(todo, adapterPosition)
     }
 }
 
-class TodoAdapter(val data: List<Todo>): RecyclerView.Adapter<TodoVH>() {
+class TodoAdapter(val data: List<Todo>, val clickListener: OnItemClickListener): RecyclerView.Adapter<TodoVH>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoVH {
         val rootView = LayoutInflater.from(parent.context).inflate(R.layout.row_default, parent, false)
-        return TodoVH(rootView)
+        return TodoVH(rootView, clickListener)
     }
 
     override fun onBindViewHolder(holder: TodoVH, position: Int) {
